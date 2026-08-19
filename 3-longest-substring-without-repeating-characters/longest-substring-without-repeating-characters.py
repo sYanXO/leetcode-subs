4c8327a689e0845b8c1a4fccf018dbc5bdda2c72
sYanXO <165569965+sYanXO@users.noreply.github.com>
2026-08-19 14:03:31 +0530

    Time: 191 ms (34.48%) | Memory: 20 MB (13.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -6,7 +6,7 @@ class Solution:
 
         for r,c in enumerate(s):
             if c in last_seen:
-                l = max(l,last_seen[c]+1)
+                l = max(l,last_seen[c]+1) # slightly faster in prac since the L ptr is jumped and not walked frward 1 by 1...yet still O(n) and O(n)
             last_seen[c] = r
             ans = max(ans,r-l+1)
         return ans
